1f79640818508d3c21da5d773d2e15ccdabaa3ff
zhongyushi <1916008067@qq.com>
2020-09-16 14:29:48 +0800

    优化项目内容

--- src/main/java/com/zys/springboottest/controller/TestController.java
+++ src/main/java/com/zys/springboottest/controller/TestController.java
@@ -1,16 +1,10 @@
 package com.zys.springboottest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author zhongyushi
- * @date 2020/9/16 0016
- * @dec 描述
- */
+
 @RestController
-@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/test")
